fdb111475fd40ea36186000337ab9adb01e4ee9c
Michael Sullivan <sullivanmichael344@gmail.com>
2023-12-31 14:07:56 -0500

    Make grid

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 const container = document.querySelector(".container")
 
+for (let i = 0; i < 16*16; i++){
+const box = document.createElement("div")
+box.classList.add("box")
+let x = 970/16;
+box.style.width = "calc(100%/16)"
+box.style.height = "calc(100%/16)"
+container.appendChild(box)
+} 
