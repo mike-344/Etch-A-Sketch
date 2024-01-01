e60493289b115c7095bf103c61627eb9a164edfa
Michael Sullivan <sullivanmichael344@gmail.com>
2023-12-31 23:33:42 -0500

    Make grid

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const container = document.querySelector(".container")
-
+let box;
 for (let i = 0; i < 16*16; i++){
-const box = document.createElement("div")
+box = document.createElement("div")
 box.classList.add("box")
-let x = 970/16;
-box.style.width = "calc(100%/16)"
-box.style.height = "calc(100%/16)"
+let x = 16;
+box.style.width = `calc(100%/${x})`
+box.style.height = `calc(100%/${x})`
 container.appendChild(box)
+box.addEventListener("mouseenter", (e) => {
+    e.target.style.backgroundColor = "black"
+})
 } 
+
+let boxes = document.querySelectorAll(".box")
+
+//for (let i = 0; i < boxes.length; i++){
+  //  boxes[i].parentNode.removeChild(boxes[i])
+//}
